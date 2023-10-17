5dde8eeaf89c8390f95840a9c0c1412455b27cb8
jeokeefe <131918720+jeokeefe@users.noreply.github.com>
2023-10-17 00:48:45 -0400

    UPDATED 10-17-2023-A

--- src/objects.js
+++ src/objects.js
@@ -1,44 +1,41 @@
-export const shallowCompare = function(obj1, obj2) {
+export const compareDates = function (objects1, objects2, index_field = 'id', date_field = 'modified_date') {
 
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
+    const object1_array = objects1;
+    if (!Array.isArray(object1_array)) return [object1_array];
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+    const object2_array = objects2;
+    let object2;
+    if (!Array.isArray(object2_array)) return [object2_array];
 
-  for (let key of keys1) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
+    for (const object1 of object1_array) {
+        if (!object1.hasOwnProperty(index_field)) return false;
+
+        const object2Index = object2_array.findIndex(obj => {
+            return String(obj[index_field]) === String(object1[index_field])}
+        );
+
+        if (object2Index === -1) return false;
+        object2 = object2_array[object2Index];
+
+        if (!_objectCompareDate(object1, object2, date_field)) return false;
+        object2_array.splice(object2Index, 1);
     }
-  }
 
-  return true;
+    return object2_array.length <= 0;
+
 }
 
-export const deepCompare = function(obj1, obj2) {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
+export const _objectCompareDate = function (object1, object2, date_field = 'modified_date') {
+    if (!_isObject(object1)) return false;
+    if (!_isObject(object2)) return false;
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+    const date1 = new Date(object1[date_field]);
+    const date2 = new Date(object2[date_field]);
 
-  for (const key of keys1) {
-    const val1 = obj1[key];
-    const val2 = obj2[key];
-    const areObjects = _isObject(val1) && _isObject(val2);
-    if (
-      areObjects && !deepCompare(val1, val2) ||
-      !areObjects && val1 !== val2
-    ) {
-      return false;
-    }
-  }
-
-  return true;
+    return date1.toString() === date2.toString();
 }
+
 
 function _isObject(object) {
-  return object != null && typeof object === 'object';
+    return object != null && typeof object === 'object';
 }
