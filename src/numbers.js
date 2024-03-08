a55de924aea5e2f55452a4d36ad6de6a699bf3de
jeokeefe <131918720+jeokeefe@users.noreply.github.com>
2024-03-08 00:22:57 -0500

    UPDATED 03-08-2024-A

--- src/numbers.js
+++ src/numbers.js
@@ -1,37 +1,48 @@
 // noinspection JSUnusedGlobalSymbols
 
-//import {getCurrentLanguageTag} from "./locales";
+import {defaultLocale} from "@xpario/javascript-tools/src/locales";
 
 const length_factor = 3.281;
 const mass_factor = 2.205;
 
 Number.prototype.covertFeetToMeters = function () {
-    return (this / length_factor);
+	return (this / length_factor);
 }
 
 Number.prototype.covertMetersToFeet = function () {
-    return (this * length_factor);
+	return (this * length_factor);
 }
 
 Number.prototype.covertLbsToKG = function () {
-    return (this / mass_factor);
+	return (this / mass_factor);
 }
 
 Number.prototype.covertKGToLbs = function () {
-    return (this * mass_factor);
+	return (this * mass_factor);
 }
 
 Number.safeToString = function (value) {
-    return (value === undefined || value === null) ? '' : value.toString();
+	return (value === undefined || value === null) ? '' : value.toString();
 }
 
-Number.prototype.toFormattedString = function (maxDecimals = 2, minDecimals = 0, languageTag = 'en-US') {
+function insertNumberSeparator(numberString, numberSeparator = ',') {
+	// Reverse the string, match groups of 3 characters, join with commas, then reverse back
+	let reversedString = numberString.split('').reverse().join('');
 
-    return Intl.NumberFormat(languageTag, {
-            maximumFractionDigits: maxDecimals,
-            minimumFractionDigits: minDecimals
-        }
-    ).format(this);
+	return numberString.split('').reverse().join('').match(/.{1,3}/g).join(numberSeparator).split('').reverse().join('');
+}
+
+Number.prototype.toLocaleString2 = function (objLocale = defaultLocale) {
+	// Split the number string into the integer and decimal parts
+
+	const digitGroupingSeparator = objLocale.digitGroupingSeparator;
+	const decimalSeparator = objLocale.decimalSeparator;
+
+	const numberString = this.toString();
+	const [integerPart, decimalPart] = numberString.split('.');
+	const formattedIntegerPart = insertNumberSeparator(integerPart, digitGroupingSeparator);
+
+	return decimalPart ? formattedIntegerPart + decimalSeparator + decimalPart : formattedIntegerPart;
 }
 
 export default Number;
--- src/strings.js
+++ src/strings.js
@@ -3,109 +3,128 @@
 import {defaultLocale} from "./locales";
 
 String.prototype.jsonMinify = function () {
-    let result = this;
-    if (this.isJSON()) {
-        result = JSON.stringify(JSON.parse(this));
-    }
-    return result;
+	let result = this;
+	if (this.isJSON()) {
+		result = JSON.stringify(JSON.parse(this));
+	}
+	return result;
 }
 
 String.prototype.truncate = function (length) {
-    let result = this;
-    let maxLength = length - 3;
+	let result = this;
+	let maxLength = length - 3;
 
-    if (this.length > maxLength) {
-        result = this.substring(0, maxLength) + '...';
-    }
+	if (this.length > maxLength) {
+		result = this.substring(0, maxLength) + '...';
+	}
 
-    return result;
+	return result;
 }
 
 String.prototype.left = function (n) {
-    return this.substring(0, n);
+	return this.substring(0, n);
 }
 
 String.prototype.right = function (n) {
-    return this.slice(-(n));
+	return this.slice(-(n));
 }
 
 String.prototype.brackets = function () {
-    return '[' + this + ']';
+	return '[' + this + ']';
 }
 
 String.prototype.format = function (values) {
-    let str = this;
-    if (!Array.isArray(values)) values = new Array(values);
-    if (Array.isArray(values)) {
-        for (let i = 0; i < values.length; i++) {
-            str = str.replace("{" + i + "}", values[i])
-        }
-    }
+	let str = this;
+	if (!Array.isArray(values)) values = new Array(values);
+	if (Array.isArray(values)) {
+		for (let i = 0; i < values.length; i++) {
+			str = str.replace("{" + i + "}", values[i])
+		}
+	}
 
-    return str;
+	return str;
 }
 
 String.prototype.isBlank = function () {
-    return (!this || /^\s*$/.test(this));
+	return (!this || /^\s*$/.test(this));
 }
 
 String.prototype.isEmpty = function () {
-    return (!this || this.length === 0);
+	return (!this || this.length === 0);
 }
 
 String.prototype.isJSON = function () {
-    let item = this;
-    item = typeof item !== "string"
-        ? JSON.stringify(item)
-        : item;
+	let item = this;
+	item = typeof item !== "string"
+		? JSON.stringify(item)
+		: item;
 
-    try {
-        item = JSON.parse(item);
-    } catch (e) {
-        return false;
-    }
+	try {
+		item = JSON.parse(item);
+	} catch (e) {
+		return false;
+	}
 
-    return typeof item === "object" && item !== null;
+	return typeof item === "object" && item !== null;
 }
 
 String.prototype.toHex = function () {
-    let result = '';
-    for (let x of this) {
-        result += Number(x.charCodeAt(0)).toString(16);
-    }
-    return result;
+	let result = '';
+	for (let x of this) {
+		result += Number(x.charCodeAt(0)).toString(16);
+	}
+	return result;
 }
 
 String.prototype.fromHex = function () {
-    let result = '';
-    for (let i = 0; i < this.length; i += 2) {
-        result += String.fromCharCode(parseInt(this.substring(i, i + 2), 16));
-    }
-    return result;
+	let result = '';
+	for (let i = 0; i < this.length; i += 2) {
+		result += String.fromCharCode(parseInt(this.substring(i, i + 2), 16));
+	}
+	return result;
 }
 
 String.prototype.escapeSingleQuotes = function () {
-    let result = this;
-    result = this.replace(/’/g, "\\\\'");
-    return result;
+	let result = this;
+	result = this.replace(/’/g, "\\\\'");
+	return result;
 }
 
-String.prototype.toFloat = function (objLocale = defaultLocale) {
+function splitDecimalString(decimalString, decimalSeparator) {
 
-    const allWhitespace = new RegExp("[\\s\\u00A0]", 'g');
-    let result = this.replace(allWhitespace, '');
+	const result = {numberPart: null, decimalPart: null};
+	// Split the decimal string into the number and decimal parts
+	const parts = decimalString.split(decimalSeparator);
 
-    const digitGroupingSeparator = new RegExp(objLocale.digitGroupingSeparator, 'g');
-    result = result.replace(digitGroupingSeparator, '');
+	if (parts.length === 0) {
+		// If there are no parts, return null object
+		return result;
+	}
 
-    result = result.replace(objLocale.currencyCode, '');
-    result = result.replace(objLocale.currencySymbol, '');
-    result = result.replace(objLocale.decimalSeparator, '.');
+	result.numberPart = parts[0].replace(/\D/g, '');
 
-    result = parseFloat(result);
+	if (parts.length > 1) {
+		result.decimalPart = parts[1].replace(/\D/g, '');
+	} else {
+		result.decimalPart = 0;
+	}
 
-    if (isNaN(result)) return null;
-    return result;
+	return result;
+}
+
+String.prototype.toLocaleFloat = function (objLocale = defaultLocale) {
+
+	if (this.isBlank()) return null;
+
+	const digitGroupingSeparator = objLocale.digitGroupingSeparator;
+	const allWhitespace = new RegExp("[\\s\\u00A0]", 'g');
+	const trimmedValue = this.replace(allWhitespace, '');
+	const objDecimal = splitDecimalString(trimmedValue, digitGroupingSeparator);
+	const strDecimal = objDecimal.numberPart.replace(/\D/g, '') + '.' + objDecimal.decimalPart;
+	const result = parseFloat(strDecimal);
+
+	if (isNaN(result)) return null;
+	return result;
 }
 
 export default String;
